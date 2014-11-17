2b4324da318e1b571f6b04e45696c085c33b5c80
Pedro Vieira <pvieira91@gmail.com>
2014-11-17 14:47:12 +0000

    Refactoring Directive

--- src/app/Directives/match.event.directive.js
+++ src/app/Directives/match.event.directive.js
@@ -1,9 +1,22 @@
 //var app = angular.module('webManagerLite',[]);
-app.directive('matchEvent', function() {
+
+define(function(){
+	
+ var _directive = function() {
     return {
-    restrict: 'E',
-	  //scope: { type: '=',player:'=', time:'=' },
-    //templateUrl: 'MatchEvent.html'
-    template: '<h3>Hello World!!</h3>'
+	    restrict: 'E',
+		  //scope: { type: '=',player:'=', time:'=' },
+	    //templateUrl: 'MatchEvent.html'
+	    template: '<h3>Hello World!!</h3>'
     };
-  });
+  };	
+	
+	
+return{
+	initialize: function(app){
+		app.directive('matchEvent', _directive);	
+	}	
+	
+};
+	
+});
