708bd42d7c62a132ade6015954f3370f745a4a59
Pedro Vieira <pvieira91@gmail.com>
2014-11-17 14:48:51 +0000

    Added directive to main module

--- src/app/app.js
+++ src/app/app.js
@@ -1,15 +1,17 @@
-define(['app/modules/match.simulator/app'],
-    function (matchSimulatorModule) {
+define(['app/modules/match.simulator/app',
+	'app/directives/match.event.directive'],
+    function (matchSimulatorModule, matchEventDirective) {
 
         var _initialize = function () {
 
 			
             //First Init Sub Modules
             matchSimulatorModule.initialize("webManagerLite.matchsimulator");
+            
 
             /*Init Main Module */
             var app = angular.module('webManagerLite', ['ngRoute', 'ngAnimate', "ui.bootstrap", "webManagerLite.matchsimulator"]);
-
+          matchEventDirective.initialize(app);
             
             app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 				$routeProvider.when('/matchsimulator', { templateUrl: 'app/modules/match.simulator/template.html', controller: 'MatchSimulatorController' });
